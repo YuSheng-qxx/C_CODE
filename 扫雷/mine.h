3f26ca0182513c22a7ee6882a9d9681dede3b080
YuSheng-qxx <1473460024@qq.com>
2020-11-12 23:13:16 +0800

    提交代码

---
+++ 扫雷/mine.h
@@ -0,0 +1,23 @@
+//#ifndef _MINE_H_
+//#define _MINE_H_
+//
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <time.h>
+//#include <windows.h>
+//
+//#pragma warning(disable:4996)
+//
+//#define ROW 12
+//#define COL 12
+//
+//#define NUM 20
+//
+//void Game();
+//void InitBoard(char board[][COL], int row, int col, char elem);
+//void SetMine(char mine[][COL], int row, int col);
+//void ShowBoard(char board[][COL], int row, int col);
+//char GetNum(char mine[][COL], int x, int y); //'0', '1', '2'
+//
+//
+//#endif
